9503f78c9a7f1b3bae6b67c008b28d877027c33b
kandaness <kandaness@163.com>
2018-10-09 23:33:29 +0800

    占位符使用

--- FoundationTypes/FoundationTypes.py
+++ FoundationTypes/FoundationTypes.py
@@ -5,3 +5,12 @@ print 'code 1):a[3]'
 print a[3]
 print 'code 2):a[-1-0]'
 print a[-1-0]
+print '2. a = "jay" b="python" 用字符串拼接的方法输出：my name is jay,i love python.'
+a='jay'
+b='python'
+print 'code 1): "my name is "+a+",i love "+b'
+print 'my name is '+a+',i love '+b
+print 'code 2): "my name is %s,i love %s" % (a,b)'
+## 第三个百分号不可省略
+print 'my name is %s,i love %s' % (a,b)
+print '3. a = "pyer" b = "apple",用字典和format方法实现：my name is pyer, i love apple.'
