30176e171501aa82f52e022bf2168ec9814c1a9c
kandaness <kandaness@163.com>
2018-11-03 22:36:43 +0800

    函数练习

--- Function/Function.py
+++ Function/Function.py
@@ -1,4 +1,7 @@
 #encoding=utf-8
+import sys
+import os
+import glob
 print '''1 写一个函数代码，返回这3个数字中最大的一个。
 a = 123
 b = 345
@@ -59,3 +62,102 @@ def cinfo(**tuple):
     return result
 list=cinfo(x=88,y=22,z=44)
 print list
+print '''3. 定义一个方法 func，该func可以引入任意多的整型参数，结果返回其中最大与最小的值
+code :
+def maxAndMin(*array):
+    for x in array:
+        if isinstance(x,int):
+            pass
+        else:
+            return "输入的参数必须为整型"
+    a=sorted(array)
+    max=a[-1]
+    min=a[0]
+    return {'max':max,'min':min}
+result=maxAndMin(4,5,2,1,6,7,8,5)
+result:'''
+def maxAndMin(*array):
+    for x in array:
+        if isinstance(x,int):
+            pass
+        else:
+            return "输入的参数必须为整型"
+    a=sorted(array)
+    max=a[-1]
+    min=a[0]
+    return {'max':max,'min':min}
+result=maxAndMin(4,5,2,1,6,7,8,5)
+print result
+print '''4. 定义一个方法func，该func可以引入任意多的字符串参数，结果返回（长度）最长的字符串
+code :
+def maxLength(*array):
+    for x in array:
+        if not isinstance(x,str):
+            return "输入的参数必须为字符串"
+    a=sorted(array,key =lambda k:len(k))
+    return a[-1]
+result=maxLength('123','3453','33333','12')
+result:'''
+def maxLength(*array):
+    for x in array:
+        if not isinstance(x,str):
+            return "输入的参数必须为字符串"
+    a=sorted(array,key =lambda k:len(k))
+    return a[-1]
+result=maxLength('123','3453','33333','12')
+print result
+print '''5. 定义一个方法get_doc(module)，module参数为该脚本中导入或定义的模块对象，该函数返回module的帮助文档
+code 1):
+def get_doc(module):
+    __import__(module)
+    my_module=sys.modules[module]
+    doc=my_module.__doc__
+    return doc
+doc=get_doc('os')
+result:'''
+def get_doc(module):
+    __import__(module)
+    my_module=sys.modules[module]
+    doc=my_module.__doc__
+    return doc
+doc=get_doc('os')
+print doc
+print '''code 2):
+def get_doc(module):
+    exec "import %s" %module
+    doc=module+".__doc__"
+    exec "print %s"%doc
+doc=get_doc('os')
+'''
+def get_doc(module):
+    exec "import %s" %module
+    doc=module+".__doc__"
+    exec "print %s" %doc
+get_doc('os')
+print '''6. 定义一个方法get_text(f),f参数为任意一个文件的磁盘路径，该函数返回f文件的内容
+code :
+def get_text(f):
+    if os.path.exists(f):
+        with open(f,'r') as g:
+            return g.read()
+    return "file isn't exists"
+doc_str=get_text('/home/joker/code/python/FoundationTypes/info.txt')
+result:'''
+def get_text(f):
+    if os.path.exists(f):
+        with open(f,'r') as g:
+            return g.read()
+    return "file isn't exists"
+doc_str=get_text('/home/joker/code/python/FoundationTypes/info.txt')
+print doc_str
+print '''定义一个方法get_dir(folder),folder参数为任意一个文件夹，该函数返回folder文件夹的文件列表
+code :
+def get_dir(folder):
+    for x in glob.glob(folder+'\*'):
+        print x
+get_dir('/home/joker/code/python')
+result:'''
+def get_dir(folder):
+    for x in glob.glob(folder+'/*'):
+        print x
+get_dir('/home/joker/code/python')
